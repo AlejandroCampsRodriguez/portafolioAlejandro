cd6bd3d92f61f1d79fa1569f402b3a05aff9a6ff
alejandro <alexcrw42@gmail.com>
2024-08-18 00:34:20 +0200

    otra modificacion

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code>  pagina portada del portafolio
         </p>
       </div>
       <p className="read-the-docs">
